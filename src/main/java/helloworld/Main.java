7c8d1738e8bddeff450fb5c165a4cb3579c65d1b
William <williamgao520@gmail.com>
2016-06-30 13:48:05 -0700

    Hello World!

---
+++ src/main/java/helloworld/Main.java
@@ -0,0 +1,16 @@
+package main.java.helloworld;
+
+/**
+ * hello-world
+ *
+ * @author w-gao Copyright (c) 2016
+ * @version 1.0
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+    }
+
+}
